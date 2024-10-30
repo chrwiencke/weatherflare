684b98d77d52dfa93f3436751a1e6906344a0f46
chrwiencke <81775007+chrwiencke@users.noreply.github.com>
2024-10-30 22:48:53 +0100

    Implement city input handling in searchCity function and improve error response

--- functions/searchCity.js
+++ functions/searchCity.js
@@ -1,15 +1,17 @@
-export async function onRequest(context) {
-    const { request } = context;
-  
-    // Create a response object with the request details
-    const requestDetails = {
-      method: request.method,
-      url: request.url,
-      headers: Object.fromEntries(request.headers),
-      body: await request.text()
-    };
-  
-    return new Response(JSON.stringify(requestDetails, null, 2), {
-      headers: { "Content-Type": "application/json" },
+async function handleRequest(request) {
+    const url = new URL(request.url);
+    const cityInput = url.searchParams.get("cityInput");
+    const APIKEY = context.env.WeatherAPIKey
+    if (cityInput) {
+    request(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${APIKEY}`)
+
+    return new Response(JSON.stringify(response), {
+        headers: { "Content-Type": "application/json" },
     });
-  }
+    } else {
+    return new Response("City input not provided", {
+        status: 400,
+        headers: { "Content-Type": "text/plain" },
+    });
+    }
+}
